6787a9e708cbb35ac56a7b40f9995a1819b51b39
Corey Farrell <git@cfware.com>
2023-10-18 19:55:32 -0400

    feat!: Update dependencies

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
+/* eslint unicorn/prefer-event-target: 0 */
 import fs from 'node:fs/promises';
 import EventEmitter from 'node:events';
 
 import pMap from 'p-map';
 
 export class IntegrationInstanceBase extends EventEmitter {
-	#resolveStopped;
-	#instancesPath;
-	#instances = {};
+    #resolveStopped;
+    #instancesPath;
+    #instances = {};
 
-	get instances() {
-		return Object.keys(this.#instances);
-	}
+    get instances() {
+        return Object.keys(this.#instances);
+    }
 
-	get defaultInstance() {
-		return this[this.instances[0]];
-	}
+    get defaultInstance() {
+        return this[this.instances[0]];
+    }
 
-	get startedMessage() {
-		return 'Started daemons';
-	}
+    get startedMessage() {
+        return 'Started daemons';
+    }
 
-	stopped = new Promise(resolve => {
-		this.#resolveStopped = resolve;
-	});
+    stopped = new Promise(resolve => {
+        this.#resolveStopped = resolve;
+    });
 
-	constructor(instancesPath, instances) {
-		super();
+    constructor(instancesPath, instances) {
+        super();
 
-		this.#instancesPath = instancesPath;
-		Object.assign(this.#instances, instances);
-		Object.assign(this, instances);
-	}
+        this.#instancesPath = instancesPath;
+        Object.assign(this.#instances, instances);
+        Object.assign(this, instances);
+    }
 
-	async build() {
-		await fs.rm(this.#instancesPath, {recursive: true, force: true});
-		this.emit('status', 'Building daemons');
-		await pMap(
-			Object.values(this.#instances),
-			instance => instance.build()
-		);
-	}
+    async build() {
+        await fs.rm(this.#instancesPath, {recursive: true, force: true});
+        this.emit('status', 'Building daemons');
+        await pMap(
+            Object.values(this.#instances),
+            instance => instance.build()
+        );
+    }
 
-	async start(...args) {
-		await this.build();
+    async start(...args) {
+        await this.build();
 
-		this.emit('status', 'Starting daemons');
-		await pMap(
-			Object.values(this.#instances),
-			instance => instance.start(...args)
-		);
-		this.emit('status', this.startedMessage);
-	}
+        this.emit('status', 'Starting daemons');
+        await pMap(
+            Object.values(this.#instances),
+            instance => instance.start(...args)
+        );
+        this.emit('status', this.startedMessage);
+    }
 
-	async stop() {
-		this.emit('status', 'Stopping daemons');
-		await pMap(
-			Object.values(this.#instances),
-			instance => instance.stop()
-		);
-		this.emit('status', 'Stopped daemons');
+    async stop() {
+        this.emit('status', 'Stopping daemons');
+        await pMap(
+            Object.values(this.#instances),
+            instance => instance.stop()
+        );
+        this.emit('status', 'Stopped daemons');
 
-		this.#resolveStopped();
-	}
+        this.#resolveStopped();
+    }
 
-	async checkStopped() {
-		await pMap(
-			Object.values(this.#instances),
-			instance => instance.checkStopped?.()
-		);
-	}
+    async checkStopped() {
+        await pMap(
+            Object.values(this.#instances),
+            instance => instance.checkStopped?.()
+        );
+    }
 }
--- test.js
+++ test.js
@@ -5,90 +5,90 @@ import t from 'libtap';
 import {IntegrationInstanceBase} from '@cfware/integration-instance-base';
 
 class CounterInstance {
-	counts = {
-		build: 0,
-		start: [],
-		stop: 0,
-		checkStopped: 0
-	};
+    counts = {
+        build: 0,
+        start: [],
+        stop: 0,
+        checkStopped: 0
+    };
 
-	async build() {
-		this.counts.build++;
-	}
+    async build() {
+        this.counts.build++;
+    }
 
-	async start(...args) {
-		this.counts.start.push(args);
-	}
+    async start(...args) {
+        this.counts.start.push(args);
+    }
 
-	async stop() {
-		this.counts.stop++;
-	}
+    async stop() {
+        this.counts.stop++;
+    }
 
-	async checkStopped() {
-		this.counts.checkStopped++;
-	}
+    async checkStopped() {
+        this.counts.checkStopped++;
+    }
 }
 
 t.type(IntegrationInstanceBase, 'function');
 
 t.test('counters', async t => {
-	const statuses = [];
-	await fs.mkdir('instances', {recursive: true});
+    const statuses = [];
+    await fs.mkdir('instances', {recursive: true});
 
-	const counter1 = new CounterInstance();
-	const counter2 = new CounterInstance();
-	const integration = new IntegrationInstanceBase(path.resolve('instances'), {counter1, counter2});
-	integration.on('status', status => statuses.push(status));
-	t.same(integration.instances, ['counter1', 'counter2']);
-	t.equal(integration.defaultInstance, counter1);
-	t.equal(integration.counter1, counter1);
-	t.equal(integration.counter2, counter2);
-	t.equal(integration.startedMessage, 'Started daemons');
-	// eslint-disable-next-line promise/prefer-await-to-then
-	integration.stopped.then(() => {
-		statuses.push('resolved stopped');
-	});
+    const counter1 = new CounterInstance();
+    const counter2 = new CounterInstance();
+    const integration = new IntegrationInstanceBase(path.resolve('instances'), {counter1, counter2});
+    integration.on('status', status => statuses.push(status));
+    t.same(integration.instances, ['counter1', 'counter2']);
+    t.equal(integration.defaultInstance, counter1);
+    t.equal(integration.counter1, counter1);
+    t.equal(integration.counter2, counter2);
+    t.equal(integration.startedMessage, 'Started daemons');
+    // eslint-disable-next-line promise/prefer-await-to-then
+    integration.stopped.then(() => {
+        statuses.push('resolved stopped');
+    });
 
-	const counts = {
-		...integration.counter1.counts,
-		start: []
-	};
-	const checkCounts = () => {
-		t.same(integration.counter1.counts, counts);
-		t.same(integration.counter2.counts, counts);
-	};
+    const counts = {
+        ...integration.counter1.counts,
+        start: []
+    };
+    const checkCounts = () => {
+        t.same(integration.counter1.counts, counts);
+        t.same(integration.counter2.counts, counts);
+    };
 
-	checkCounts();
+    checkCounts();
 
-	await integration.build();
-	counts.build++;
-	checkCounts();
-	t.same(statuses, ['Building daemons']);
-	statuses.length = 0;
+    await integration.build();
+    counts.build++;
+    checkCounts();
+    t.same(statuses, ['Building daemons']);
+    statuses.length = 0;
 
-	await integration.start('arg1', 'arg2');
-	counts.build++;
-	counts.start.push(['arg1', 'arg2']);
-	checkCounts();
-	t.same(statuses, [
-		'Building daemons',
-		'Starting daemons',
-		integration.startedMessage
-	]);
-	statuses.length = 0;
+    await integration.start('arg1', 'arg2');
+    counts.build++;
+    counts.start.push(['arg1', 'arg2']);
+    checkCounts();
+    t.same(statuses, [
+        'Building daemons',
+        'Starting daemons',
+        integration.startedMessage
+    ]);
+    statuses.length = 0;
 
-	await integration.stop();
-	counts.stop++;
-	checkCounts();
-	t.same(statuses, [
-		'Stopping daemons',
-		'Stopped daemons',
-		'resolved stopped'
-	]);
-	statuses.length = 0;
+    await integration.stop();
+    counts.stop++;
+    checkCounts();
+    t.same(statuses, [
+        'Stopping daemons',
+        'Stopped daemons',
+        'resolved stopped'
+    ]);
+    statuses.length = 0;
 
-	await integration.checkStopped();
-	counts.checkStopped++;
-	checkCounts();
-	t.same(statuses, []);
+    await integration.checkStopped();
+    counts.checkStopped++;
+    checkCounts();
+    t.same(statuses, []);
 });
